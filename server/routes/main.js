5f83f57971822ed7ad4e2d0ed71f36773be9e71c
Nguyen Van Khanh Nhan <nhanhoa21012002@gmail.com>
2023-09-13 19:45:05 +0700

    add about file and custom title

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Routes
 router.get("/", (req, res) => {
-  res.render("index");
+  const data = {
+    title: "Home",
+    description: "This is the home page",
+  };
+  res.render("index", { data });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
